eb3c6c0220d999814b99379314a84e8cafe7618f
nikhilgoenkatech <nikhil.goenka@gmail.com>
2021-07-23 05:23:21 +0000

    Fixed routing issue

--- routes/register.js
+++ routes/register.js
@@ -4,16 +4,16 @@ var User = require('../models/user');
 var mongoose = require('mongoose');
 
 /* Get register page. */
-router.get('./', function(req, res, next) {
+router.get('/', function(req, res, next) {
   if(req.cookies.logged){
-  	res.redirect('./member');
+  	res.redirect('/member');
   } else {
   	res.render('register', { title: 'Register' });
   }
 });
 
 /* Proccess Register*/
-router.post('./', function(req, res, next) {
+router.post('/', function(req, res, next) {
   var user = new User({
   	name: req.body.name,
   	card: req.body.card,
@@ -22,6 +22,6 @@ router.post('./', function(req, res, next) {
   });
 
   user.save();
-  res.render('./login', { title: 'Login' });
+  res.render('/login', { title: 'Login' });
 });
 module.exports = router;
