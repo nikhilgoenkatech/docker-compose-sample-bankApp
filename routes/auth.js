7157d704969967a38f7ab3d89b0399f656562ab8
Hao Yin <38637525+QH-YIN@users.noreply.github.com>
2021-09-28 14:20:03 +1000

    Update auth.js
    Revert changes

--- routes/auth.js
+++ routes/auth.js
@@ -42,7 +42,7 @@ router.post('/', function(req, res, next) {
       res.status(500);
   	  res.render('index', { title: '    Invalid Credentials' });    
       console.log('User has entered invalid credentials');
-      logger.info(user.email + ' has entered invalid login credentials');
+      logger.info('The user has entered invalid login credentials');
       logger.info('500 HTTP error');
 
   }
